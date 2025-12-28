6d9be762b32fd298480bb886791db0e4bfca5dce
Ying Zhong <0x00eeee@gmail.com>
2025-12-28 08:44:05 +0800

    Integrate SwiftLint (#36)

---
+++ BuildTools/Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "BuildTools",
+  platforms: [
+    .macOS(.v15),
+  ],
+  products: [
+    .plugin(name: "SwiftLint", targets: ["SwiftLint"]),
+  ],
+  targets: [
+    .binaryTarget(
+      name: "SwiftLintBinary",
+      url: "https://github.com/realm/SwiftLint/releases/download/0.62.2/SwiftLintBinary.artifactbundle.zip",
+      checksum: "3047357eee0838a0bafc7a6e65cd1aad61734b30d7233e28f3434149fe02f522"
+    ),
+    .plugin(
+      name: "SwiftLint",
+      capability: .buildTool(),
+      dependencies: ["SwiftLintBinary"]
+    ),
+  ]
+)
