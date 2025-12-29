f0a08de5a21a8d124505918ee9043dde42c07bda
Ying Zhong <0x00eeee@gmail.com>
2025-12-29 09:39:53 +0800

    Improve translate window displaying

--- TextGrabber2/Sources/Translator.swift
+++ TextGrabber2/Sources/Translator.swift
@@ -12,17 +12,23 @@ enum Translator {
   static func showWindow(text: String) {
     NSApp.activate()
     contentVC?.setValue(NSAttributedString(string: text), forKey: "text")
+
+    windowController.window?.alphaValue = 0
     windowController.showWindow(nil)
 
     DispatchQueue.main.asyncAfter(deadline: .now() + 0.02) {
       windowController.window?.center()
+      NSAnimationContext.runAnimationGroup { context in
+        context.duration = 0.2
+        windowController.window?.animator().alphaValue = 1
+      }
     }
   }
 
   // MARK: - Private
 
-  private static var contentVC = controllerClass?.init()
-  private static var windowController = {
+  private static let contentVC = controllerClass?.init()
+  private static let windowController = {
     let window = NSWindow(contentViewController: contentVC ?? NSViewController())
     window.styleMask = [.closable, .titled]
     window.title = ""
