afe0eef89b7f131c8ec96c1c0e63a36740931da3
Ying Zhong <0x00eeee@gmail.com>
2025-12-31 10:00:42 +0800

    (Re) handle in-app keyboard events

--- TextGrabber2/Sources/App.swift
+++ TextGrabber2/Sources/App.swift
@@ -300,6 +300,26 @@ extension App {
       updateObserver(isEnabled: true)
     }
 
+    // Handle in-app keyboard events
+    NSEvent.addLocalMonitorForEvents(matching: .keyDown) { event in
+      let keyCode = event.keyCode
+      let flags = event.modifierFlags.intersection(.deviceIndependentFlagsMask)
+
+      // Cmd-Q (should be handled by menu action, just in case)
+      if keyCode == 0x0C && flags == .command {
+        NSApp.terminate(nil)
+        return nil
+      }
+
+      // Cmd-W (mainly for windows like translate)
+      if keyCode == 0x0D && flags == .command {
+        NSApp.keyWindow?.close()
+        return nil
+      }
+
+      return event
+    }
+
     // Observe clicks on the status item
     NSEvent.addLocalMonitorForEvents(matching: [.leftMouseDown, .rightMouseDown]) { [weak self] event in
       guard event.window == self?.statusItem.button?.window else {
